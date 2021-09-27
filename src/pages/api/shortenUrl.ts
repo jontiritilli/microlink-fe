9e8ee5b007553057ed80ad825ed419d16d7ff538
jontiritilli <jonathantiritilli@gmail.com>
2021-09-27 10:23:38 -0700

    push to build

--- src/pages/api/shortenUrl.ts
+++ src/pages/api/shortenUrl.ts
@@ -14,7 +14,7 @@ export default async function handler(
     const response = await axios.post(`https://microlink-n6adm7jnaq-uc.a.run.app/url/shorten`, {
       longUrl: req.body.longUrl,
     });
-    res.status(200).json({ shortUrl: response.data.shortUrl || `` });
+    res.status(200).json({ shortUrl: response.data.shortUrl || '' });
   } catch (error: any) {
     res.status(error.status || 500).end(error);
   }
